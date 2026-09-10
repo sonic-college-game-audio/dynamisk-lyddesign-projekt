eb0d4a13cc85b92fa6cb1da230fce04e5a234864
Lukas Wendt <lukwendt@gmail.com>
2026-09-10 13:38:54 +0200

    Added missing flag flip in start gate

--- Assets/Scripts/Systems/StartGate.cs
+++ Assets/Scripts/Systems/StartGate.cs
@@ -56,5 +56,7 @@ public class StartGate : MonoBehaviour
         
         distanceToPath.BeginTrackingDistanceToPath();
         Game.currentLevel.ReportEnterStartGate();
+
+        hasBeenTriggered = true;
     }
 }
